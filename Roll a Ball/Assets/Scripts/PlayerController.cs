0f2b9096bd17407205c2b9b04cb3e2ccc26e7ab0
tram.huynhthibich <tram.huynhthibich@gameloft.com>
2020-05-15 12:02:45 +0700

    + Step 7: Displaying Score and Text + Click to create new pick up item

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rBody;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public GameObject prefabObject;
+    public GameObject pickUps;
+    public Vector3 worldPosition;
+    public GameObject ground;
+    public LayerMask groundMask;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
-    }
-
+        count = 0;
+        
+        SetCountText(); 
+        winText.text = "";
+    }    
+    
     // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonUp(0)){                        
+            RaycastHit hitPoint = new RaycastHit();                       
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);            
+            
+            if (Physics.Raycast(ray, out hitPoint, 1000.0f, groundMask))
+                Instantiate(prefabObject, new Vector3(hitPoint.point.x, 0.5f, hitPoint.point.z),  Quaternion.identity, pickUps.transform);
+
+            // Plane plane = new Plane(ground.transform.position, 0);
+            // float distance;
+            // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // if (plane.Raycast(ray, out distance))
+            // {
+            //     worldPosition = ray.GetPoint(distance);
+            //     Instantiate(prefabObject, new Vector3(worldPosition.x, 0.5f, worldPosition.z),  Quaternion.identity, pickUps.transform);
+            // }            
+        }
+    }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -24,8 +55,19 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Pick Up")) {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+            winText.text = "You Win!";
+    }
+
 }
