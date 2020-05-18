0018042aeb8fadc4d8ccbf4edc3df2fa9bc418a1
tram.huynhthibich <tram.huynhthibich@gameloft.com>
2020-05-18 12:54:28 +0700

    + pool object

---
+++ Roll a Ball/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager
+{
+    public static int MAX_OBJECT_NUM = 50;
+    public List<GameObject> objectList;
+
+    public ObjectManager() {
+        objectList = new List<GameObject>();
+    }
+
+    public void AddNewObject(GameObject _object) {
+        if(objectList.Count < MAX_OBJECT_NUM) {
+            _object.SetActive(false);            
+            objectList.Add(_object);
+        }        
+    }
+
+    public void ActiveObject(Vector3 position) {        
+        for(int i = 0; i < objectList.Count; i++) {
+            if (!objectList[i].activeSelf) {
+                objectList[i].SetActive(true);
+                objectList[i].transform.position = position;
+                break;
+            }
+        }
+    }
+
+    public void StoreObject(GameObject _object) {
+        _object.SetActive(false);
+        _object.transform.position = Vector3.zero;
+    }
+}
